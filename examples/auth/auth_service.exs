58ad8b6eb8395d9d97ed8a65127bc3e3280d9582
mtanzi <tanzi.marco@gmail.com>
2015-08-05 16:51:15 +0100

    refactor callee using GenServer

--- examples/auth/auth_service.exs
+++ examples/auth/auth_service.exs
@@ -4,57 +4,39 @@
 
 defmodule Auth do
   @moduledoc """
-  The `RPC` module implements the example for asynchronous Remote Procedure Call.
+  The `Auth` module implements the example for a login RPC.
   """
 
   defmodule Callee do
+    use GenServer
+
     require Logger
-    defstruct [:callee, :procedure, timeout: 1000]
+    defstruct [:callee]
 
     # Public Functions
 
-    @doc """
-    Start the callee with the passed procedures.
-    """
-    def start_link(procedures, options \\ []) do
-      {:ok, spawn_link(fn -> init(procedures, options) end)}
+    def start_link(procedure, options \\ []) do
+      GenServer.start_link(__MODULE__, {procedure, options}, name: __MODULE__)
     end
+
+    def stop, do: GenServer.cast(__MODULE__, :stop)
 
     # Private Functions
 
-    defp init(procedure, options) do
+    def init({procedure, options}) do
       {:ok, callee} = Auth.new_peer([Spell.Role.Callee], options)
-      {:ok, _register_id} = Spell.cast_register(callee, procedure)
+      {:ok, _register_id} = Spell.call_register(callee, procedure)
 
-      %__MODULE__{callee: callee,
-                  procedure: procedure}
-        |> struct(options)
-        |> loop()
+      state = %__MODULE__{callee: callee}
+      {:ok, state}
     end
 
-    defp loop(state) do
-      receive do
-        :stop -> :ok
-        {Spell.Peer, pid, message} ->
-          handle_message(state, message)
-          loop(state)
-      after
-        state.timeout -> {:error, :timeout}
-      end
-    end
+    def handle_cast(:stop, state), do: {:stop, :normal, state}
 
-    defp handle_message(state, %Spell.Message{args: _args, code: _code, type: :registered}) do
-      Logger.info("Function registered")
+    def handle_info({Spell.Peer, _pid, %Spell.Message{args: [request, _reg_id, _msg, params], type: :invocation}}, (%{callee: callee}) = state) do
+      Spell.cast_yield(callee, request, [arguments_kw: Poison.Parser.parse!(params) |> Auth.login])
+      {:noreply, state}
     end
-
-    defp handle_message(state, %Spell.Message{args: [request, _reg_id, _msg], code: code, type: :invocation} = message) do
-      Spell.cast_yield(state.callee, request)
-    end
-
-    defp handle_message(state, %Spell.Message{args: [request, _reg_id, _msg, params], code: code, type: :invocation} = message) do
-      Spell.cast_yield(state.callee, request, [arguments_kw: Poison.Parser.parse!(params) |> Auth.login])
-    end
-
   end
 
   @doc """
@@ -86,7 +68,7 @@ Logger.info("Starting the Crossbar.io test server...")
 {:ok, _pid} = Crossbar.start()
 
 procedure = "api.mymedsandme.auth"
-{:ok, callee} = Callee.start_link(procedure)
+{:ok, _callee} = Callee.start_link(procedure)
 
 :timer.sleep(1000000)
 
